0f3eac668749479f73d62c5017ba95e3a9012029
victordomin <dominvic@gmail.com>
2016-12-09 20:50:50 -0600

    Adds four directories on text encoding

---
+++ 01_getting-started/02_numeral-systems/02_binary/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 42
+	fmt.Printf("%d - %b\n", x, x)
+}
---
+++ 01_getting-started/02_numeral-systems/03_hexadecimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 4096
+	fmt.Printf("%d - %b - %x - %#x\n", x, x, x, x)
+}
